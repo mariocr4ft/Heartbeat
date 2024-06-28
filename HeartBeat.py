9ef60b11e2d756592d7bc02eeb7e093ff29591d0
Mariocraft <mariocraftuwu@gmail.com>
2024-06-28 11:11:09 +0200

    Initial commit

---
+++ HeartBeat.py
@@ -0,0 +1,45 @@
+import requests
+import os
+import platform
+import distro
+import schedule
+import time
+import json
+
+
+osType = platform.system()
+
+usingLinux = False
+osdistro = ""
+osver = ""
+url = ""
+
+def runningTimer():
+    response = requests.get(url)
+    status_code = requests.status_codes
+    if response.status_code == 200:
+        print("Heartbeat connected correctly. Code 200.")
+    else:
+        print("Error connecting to the heartbeat. Please check the firewall or have this code as a reference. Code: "+status_code)
+
+with open('config.json', 'r') as file:
+    data = json.load(file)
+    url = data['url']
+
+if osType == "Linux":
+    usingLinux = True
+    osdistro = distro.name()
+    osver = distro.version()
+
+if not usingLinux:
+    print("Hello! You are supposed to use this script with Linux only, otherwise, the script will fail.")
+    print("If you think this is an error, please report it at https://github.com/mariocr4ft/Heartbeat/issues")
+    exit()
+else:
+    print(f"You are using Linux {osdistro} version {osver}.")
+    schedule.every(10).minutes.do(runningTimer)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
